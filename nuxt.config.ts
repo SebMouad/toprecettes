9df62cafb16b7ff3310a7cb53309a15ccb3e5e00
Mouad <132716054+SebMouad@users.noreply.github.com>
2023-06-12 22:42:30 +0100

    Init

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-16",
+      viewport: "width=device-width,initial-scale=1",
+      title: "Top Recettes",
+      titleTemplate: "%s - Top Recettes",
+      meta: [{ name: "description", content: "Des recettes faciles pour des repas sains et savoureux !" }],
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    public: {
+      wpUri: process.env.WP_URI,
+    },
+  },
+});
